681a1c279608e1e4d508df6b7542daa672c676a0
Ayush Srivastava <constayush@gmail.com>
2026-06-27 10:32:25 +0530

    email bug fix: forces all DNS looks in  IPV4 as IPV6 not available at render

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,4 +1,7 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
