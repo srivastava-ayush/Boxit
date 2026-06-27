e5beefee022fd03effe3c20c92ca16411ef48082
Ayush Srivastava <constayush@gmail.com>
2026-06-27 11:24:20 +0530

    changed STMP family to 4

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -7,6 +7,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT) || 465,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
