905c2bcb15fdca53d4b176b73e5588ee6ed6d54b
Ayush Srivastava <constayush@gmail.com>
2026-06-27 10:45:15 +0530

    SMPT port changed to 465

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -5,7 +5,8 @@ dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
+  port: Number(process.env.SMTP_PORT) || 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
